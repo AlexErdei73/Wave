f812b995ff0a66d7384c7a216cc032a50b3b4a5e
Alex Erdei <mralexerdei@yahoo.co.uk>
2023-12-21 00:53:03 +0000

    Make plot in main method

--- src/Wave.java
+++ src/Wave.java
@@ -1,5 +1,8 @@
 public class Wave {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Plot plot = new Plot("Sin Function", 0, 4 * Math.PI, Math.PI / 18, -2, 2, 0.1);
+        for (double angle = 0; angle <= 4 * Math.PI; angle += 0.004 * Math.PI) {
+            plot.addPoint(angle, Math.sin(angle));
+        }
     }
 }
