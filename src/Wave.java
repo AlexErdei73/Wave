283b631cda6359e6ef1273807819e700fa2c5fcc
Alex Erdei <mralexerdei@yahoo.co.uk>
2023-12-21 19:29:27 +0000

    Add wave1 and plot it

--- src/Wave.java
+++ src/Wave.java
@@ -1,8 +1,11 @@
 public class Wave {
+    static double wave1(double x) {
+        return Math.sin(x) + Math.sin(3*x)/3;
+    }
     public static void main(String[] args) {
         Plot plot = new Plot("Sin Function", 0, 4 * Math.PI, Math.PI / 18, -2, 2, 0.1);
         for (double angle = 0; angle <= 4 * Math.PI; angle += 0.004 * Math.PI) {
-            plot.addPoint(angle, Math.sin(angle));
+            plot.addPoint(angle, wave1(angle));
         }
     }
 }
