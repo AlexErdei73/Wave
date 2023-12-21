b8f8b1d8305f7359b198b43692c57abbfa7a8ec0
Alex Erdei <mralexerdei@yahoo.co.uk>
2023-12-21 21:44:36 +0000

    Sum up the series in wave1 with for loop

--- src/Wave.java
+++ src/Wave.java
@@ -1,10 +1,16 @@
 public class Wave {
     static double wave1(double x) {
-        return Math.sin(x) + Math.sin(3*x)/3;
+        double sum = 0;
+        int kmax = 100;
+        for (int k = 1; k <= kmax; k += 2) {
+            sum += Math.sin(k*x) / k;
+        }
+        return sum;
     }
     public static void main(String[] args) {
         Plot plot = new Plot("Sin Function", 0, 4 * Math.PI, Math.PI / 18, -2, 2, 0.1);
-        for (double angle = 0; angle <= 4 * Math.PI; angle += 0.004 * Math.PI) {
+        plot.setPointSize(1);
+        for (double angle = 0; angle <= 4 * Math.PI; angle += 0.00004 * Math.PI) {
             plot.addPoint(angle, wave1(angle));
         }
     }
